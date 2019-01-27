5e5f79c9b97f1bbbd9f4cb4904143bda96e82f54
kmmoney <47075041+kmmoney@users.noreply.github.com>
2019-01-27 17:56:19 +0900

    Add files via upload

--- day01.R
+++ day01.R
@@ -21,3 +21,43 @@ name <- c("김아무개","박아무개","이아무개")
 name[2] <- "명광민"
 print(name)
 
+# facter구분하기
+
+address <- c("서울", "대구", "부산", "군산", "서울", "서울", "대구")
+str(address)
+chr [1:7] "서울" "대구" "부산" "군산" "서울" "서울" "대구")
+ 
+address <- c("서울", "대구", "부산", "군산", "서울", "서울", "대구")
+str(address)
+
+address2 <- as.factor(address)
+str(address2)
+
+address2[1] <-  "대구"
+address2[1] <-  "광주"
+
+# 입력의 다양한 형태
+x <- scan()
+x
+
+x2 <- scan()
+x2[2] <- 222
+x2
+
+str(x2)
+class(x2)
+typeof(x2)
+
+
+food <- scan(what = "")
+food
+
+str(food)
+class(food)
+typeof(food)
+
+favorite <- food[1]
+cat("내가 제일 좋아하는 음식은", favorite)
+
+second <- food[2]
+glue("나의 {second} 음식은 2번째로 좋아하는 음식이에요.")
