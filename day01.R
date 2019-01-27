e9b5f9989ddd1ac8fdf3a53ced24cb2b41c2863d
kmmoney <47075041+kmmoney@users.noreply.github.com>
2019-01-27 16:30:25 +0900

    Add files via upload

---
+++ day01.R
@@ -0,0 +1,23 @@
+install.packages("glue")
+install.packages("ggplot2")
+
+library(glue)
+
+# 여기에 glue를 이용해서 프린트연습
+# 
+# temp 변수 : 아무것도 안붙어있다
+# 할당 연산자 (알트+-)
+평균 %>%  정렬 %>%  프린트
+
+ages <- c(10, 30, 50)
+ages[1]
+ages[1] <- 
+
+  # 이름 벡터를 만들고
+  # 두번째 사람이 박아무개로 개명
+  # 이름의 전체 내용을 출력
+  
+name <- c("김아무개","박아무개","이아무개")
+name[2] <- "명광민"
+print(name)
+
