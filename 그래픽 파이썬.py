05d2acb2491304766d6259f07ab70d4872216b2b
kmmoney <47075041+kmmoney@users.noreply.github.com>
2019-03-03 13:21:34 +0900

    Add files via upload

---
+++ 그래픽 파이썬.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# 이것은 파이썬 시작 파일 입니다.
+# 그래픽을 간단하게 파이썬으로 구현해볼 예정입니다
+
+
+# In[7]:
+
+
+#1. 필요한 패키기 설치
+#2. 패키지 사용
+
+from tkinter import *
+
+
+# In[8]:
+
+
+window = Tk() #윈도우 만들어주는 함수
+
+
+# In[9]:
+
+
+window.title("나의 첫 번째 윈도우")
+
+
+# In[11]:
+
+
+window.geometry("800x600")
+
+
+# In[ ]:
+
+
+window.mainloop()
+
